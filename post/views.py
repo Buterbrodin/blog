8885ea480e6242f80c252335f64004de51fbaec1
kiril <kirill.samal@gmail.com>
2025-09-19 12:31:26 +0300

    Added interraction with posts, templates for authentication

--- post/views.py
+++ post/views.py
@@ -1,14 +1,19 @@
 from django.contrib.messages import success
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
+from django.contrib import messages
 from .models import Post
 from .forms import PostForm
 
 
 def home(request):
     posts = Post.objects.all()
-    return render(request, 'home.html', {'posts': posts})
+    status = {'info': 'primary', 'success': 'success', 'error': 'danger'}
+    icons = {'info': 'bi-info-circle',
+             'success': 'bi-check-circle',
+             'error': 'bi-exclamation-triangle'}
+    return render(request, 'home.html', {'posts': posts, 'status': status, 'icons': icons})
 
 
 def about(request):
@@ -18,31 +23,32 @@ def about(request):
 
 
 def edit(request, slug):
-    post = Post.objects.get(slug=slug)
-    success = False
+    post = get_object_or_404(Post, slug=slug)
     if request.method == 'GET':
         form = PostForm(instance=post)
     else:
         form = PostForm(request.POST, instance=post)
         if form.is_valid():
             form.save()
-            success = True
-    return render(request, 'edit.html', {'post': post, 'form': form, 'success': success})
+            messages.add_message(request, messages.SUCCESS, 'The post was successfully edited!')
+            return redirect('/')
+    return render(request, 'edit.html', {'post': post, 'form': form})
 
 
 def create(request):
-    success = False
     if request.method == 'GET':
         form = PostForm()
     else:
         form = PostForm(request.POST)
         if form.is_valid():
             form.save()
-            success = True
-    return render(request, 'create.html', {'form': form, 'success': success})
+            messages.add_message(request, messages.SUCCESS, 'The post was successfully created!')
+            return redirect('/')
+    return render(request, 'create.html', {'form': form})
 
 
 def delete(request, slug):
-    post = Post.objects.get(slug=slug)
+    post = get_object_or_404(Post, slug=slug)
     post.delete()
-    return render(request, 'delete.html')
+    messages.add_message(request, messages.SUCCESS, 'The post was successfully deleted!')
+    return redirect('/')
