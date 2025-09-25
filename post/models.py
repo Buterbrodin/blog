098bafad17786313af5fae8b5ba2d0353f5a443f
kiril <kirill.samal@gmail.com>
2025-09-25 14:30:46 +0300

    Implemented CBV, added Comment system

--- post/models.py
+++ post/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils.text import slugify
 from taggit.managers import TaggableManager
 from django.contrib.auth.models import User
-from taggit.models import Tag
+from django.urls import reverse
 
 
 class Post(models.Model):
@@ -25,7 +25,32 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+    def get_absolute_url(self):
+        return reverse('about', args=[self.slug])
+
     def save(self, *args, **kwargs):
         if not self.slug:
-            self.slug = slugify(self.title)
+            base_slug = slugify(self.title)
+            slug = base_slug
+            counter = 1
+            while Post.objects.filter(slug=slug).exists():
+                slug = f'{base_slug}-{counter}'
+                counter += 1
+            self.slug = slug
         super().save(*args, **kwargs)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments', null=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments', null=True)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+        ordering = ['-created_at']
+        indexes = [
+            models.Index(fields=['created_at', 'post'])
+        ]
--- post/views.py
+++ post/views.py
@@ -1,16 +1,13 @@
-from django.contrib.messages import success
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
-from django.shortcuts import get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
-from .models import Post
-from .forms import PostForm, CustomLoginForm, CustomRegisterForm, CustomPasswordChangeForm
-from django.contrib.auth.views import LoginView, PasswordResetView, LogoutView, PasswordChangeView
+from .models import Post, Comment
+from .forms import PostForm, CustomLoginForm, CustomRegisterForm, CustomPasswordChangeForm, CommentForm
+from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView
 from django.core.paginator import Paginator
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from django.urls import reverse_lazy
-from django.contrib import messages
-from django.contrib.messages import get_messages
+from django.views.generic import DeleteView, DetailView, ListView
+from django.urls import reverse
 
 status = {'info': 'primary', 'success': 'success', 'error': 'danger'}
 icons = {'info': 'bi-info-circle',
@@ -18,67 +15,180 @@ icons = {'info': 'bi-info-circle',
          'error': 'bi-exclamation-triangle'}
 
 
-def home(request):
-    if request.GET.get('user_posts') == 'true' and request.user.is_authenticated:
-        posts = Post.objects.filter(author=request.user)
-    elif request.GET.get('tag'):
-        tag = request.GET.get('tag')
-        posts = Post.objects.filter(tags__name__contains=tag)
-    elif request.GET.get('title'):
-        posts = Post.objects.filter(title__icontains=request.GET['title'])
-    else:
-        posts = Post.objects.all()
-    paginator = Paginator(posts, 5)
-    page_number = request.GET.get('page')
-    posts = paginator.get_page(page_number)
-    return render(request, 'post/home.html', {'posts': posts, 'status': status, 'icons': icons})
+class HomeView(ListView):
+    '''Home page view'''
+    model = Post
+    template_name = 'post/home.html'
+    context_object_name = 'posts'
+    paginate_by = 5
+
+    def get_queryset(self):
+        qs = Post.objects.all()
+        if self.request.GET.get('user_posts') == 'true' and self.request.user.is_authenticated:
+            qs = qs.filter(user=self.request.user)
+        elif self.request.GET.get('tag'):
+            tag = self.request.GET.get('tag')
+            qs = qs.filter(tag__name__contains=tag)
+        elif self.request.GET.get('title'):
+            title = self.request.GET.get('title')
+            qs = qs.filter(title__icontains=title)
+        return qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['status'] = status
+        context['icons'] = icons
+        return context
 
 
-def view_user_posts(request):
-    posts = Post.objects.filter(author=request.user)
+# def home(request):
+#     if request.GET.get('user_posts') == 'true' and request.user.is_authenticated:
+#         posts = Post.objects.filter(author=request.user)
+#     elif request.GET.get('tag'):
+#         tag = request.GET.get('tag')
+#         posts = Post.objects.filter(tags__name__contains=tag)
+#     elif request.GET.get('title'):
+#         posts = Post.objects.filter(title__icontains=request.GET['title'])
+#     else:
+#         posts = Post.objects.all()
+#     paginator = Paginator(posts, 5)
+#     page_number = request.GET.get('page')
+#     posts = paginator.get_page(page_number)
+#     return render(request, 'post/home.html', {'posts': posts, 'status': status, 'icons': icons})
 
 
-def about(request, slug):
-    post = get_object_or_404(Post, slug=slug)
-    return render(request, 'post/about.html', {'post': post})
+class PostDetailView(DetailView):
+    '''Post detail view'''
+    model = Post
+    template_name = 'post/about.html'
+    context_object_name = 'post'
+    slug_field = 'slug'
+    slug_url_kwarg = 'slug'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['status'] = status
+        context['icons'] = icons
+        return context
 
 
-def edit(request, slug):
-    post = get_object_or_404(Post, slug=slug)
-    if request.method == 'GET':
-        form = PostForm(instance=post)
-    else:
-        form = PostForm(request.POST, instance=post)
-        if form.is_valid():
-            form.save()
-            messages.add_message(request, messages.SUCCESS, 'The post was successfully edited!')
-            return redirect('/')
-    return render(request, 'post/edit.html', {'post': post, 'form': form})
+# def about(request, slug):
+#     post = get_object_or_404(Post, slug=slug)
+#     return render(request, 'post/about.html', {'post': post})
 
 
-def create(request):
-    form = PostForm()
-    if request.method == 'POST':
-        form = PostForm(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.save()
-            form.save_m2m()
-            messages.add_message(request, messages.SUCCESS, 'The post was successfully created!')
-            return redirect('/')
-    return render(request, 'post/create.html', {'form': form})
+class EditPostView(UpdateView):
+    '''Edit post view'''
+    model = Post
+    form_class = PostForm
+    template_name = 'post/edit.html'
+    success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        messages.success(self.request, 'The post was successfully edited!')
+        return super().form_valid(form)
 
 
-def delete(request, slug):
-    if request.method == 'POST':
-        post = get_object_or_404(Post, slug=slug)
-        post.delete()
-        messages.add_message(request, messages.SUCCESS, 'The post was successfully deleted!')
-    return redirect('/')
+# def edit(request, slug):
+#     post = get_object_or_404(Post, slug=slug)
+#     if request.method == 'GET':
+#         form = PostForm(instance=post)
+#     else:
+#         form = PostForm(request.POST, instance=post)
+#         if form.is_valid():
+#             form.save()
+#             messages.add_message(request, messages.SUCCESS, 'The post was successfully edited!')
+#             return redirect('/')
+#     return render(request, 'post/edit.html', {'post': post, 'form': form})
+
+
+class PostCreateView(CreateView):
+    '''Post create view'''
+    model = Post
+    form_class = PostForm
+    success_url = reverse_lazy('home')
+    template_name = 'post/create.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        messages.success(self.request, 'The post was successfully created!')
+        return super().form_valid(form)
+
+
+# def create(request):
+#     form = PostForm()
+#     if request.method == 'POST':
+#         form = PostForm(request.POST)
+#         if form.is_valid():
+#             post = form.save(commit=False)
+#             post.author = request.user
+#             post.save()
+#             form.save_m2m()
+#             messages.add_message(request, messages.SUCCESS, 'The post was successfully created!')
+#             return redirect('/')
+#     return render(request, 'post/create.html', {'form': form})
+
+
+class PostDeleteView(DeleteView):
+    '''Post delete view'''
+    model = Post
+    success_url = reverse_lazy('home')
+
+    def post(self, request, *args, **kwargs):
+        messages.success(request, "The post was successfully deleted!")
+        return super().post(request, *args, **kwargs)
+
+
+# def delete(request, slug):
+#     if request.method == 'POST':
+#         post = get_object_or_404(Post, slug=slug)
+#         post.delete()
+#         messages.add_message(request, messages.SUCCESS, 'The post was successfully deleted!')
+#     return redirect('/')
+
+
+class CommentCreateView(CreateView):
+    model = Comment
+    form_class = CommentForm
+    template_name = 'post/comment_add.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        form.instance.post = get_object_or_404(Post, slug=self.kwargs['slug'])
+        messages.success(self.request, 'The comment was successfully added!')
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('about', args=(self.kwargs['slug'],))
+
+
+class CommentDeleteView(DeleteView):
+    model = Comment
+
+    def post(self, request, *args, **kwargs):
+        messages.success(self.request, 'The comment was successfully deleted!')
+        return super().post(request, *args, **kwargs)
+
+    def get_success_url(self):
+        return reverse('about', args=(self.object.post.slug,))
+
+
+# def comment_add(request, slug):
+#     form = CommentForm()
+#     if request.method == 'POST':
+#         form = CommentForm(request.POST)
+#         if form.is_valid():
+#             comment = form.save(commit=False)
+#             comment.author = request.user
+#             comment.post = get_object_or_404(Post, slug=slug)
+#             comment.save()
+#             messages.success(request, 'The comment was successfully added!')
+#             return redirect('about', slug=slug)
+#     return render(request, 'post/comment_add.html', {'form': form})
 
 
 class CustomLoginView(LoginView):
+    '''Custom login view'''
     authentication_form = CustomLoginForm
     template_name = 'registration/login.html'
     success_url = reverse_lazy('home')
@@ -89,6 +199,7 @@ class CustomLoginView(LoginView):
 
 
 class CustomRegisterView(CreateView):
+    '''Custom register view'''
     form_class = CustomRegisterForm
     template_name = "registration/register.html"
     success_url = reverse_lazy('home')
@@ -99,12 +210,15 @@ class CustomRegisterView(CreateView):
 
 
 class CustomLogoutView(LogoutView):
+    '''Custom logout view'''
+
     def dispatch(self, request, *args, **kwargs):
         messages.success(request, "You have been logged out!")
         return super().dispatch(request, *args, **kwargs)
 
 
 class CustomPasswordChangeView(PasswordChangeView):
+    '''Custom password change view'''
     form_class = CustomPasswordChangeForm
     template_name = 'registration/password_change.html'
     success_url = reverse_lazy('home')
